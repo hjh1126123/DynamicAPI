ffdd8404395d30c92be18df9c06ba13e4988ca7e
Jeho.Huang <945664458@qq.com>
2019-04-26 01:49:01 +0800

    顶顶顶
    点赞

--- Server/DBLocal/Mix/ApiAndData.cs
+++ Server/DBLocal/Mix/ApiAndData.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Server.DBLocal
 {
     public class ApiAndData : DBComponent
     {
-        public object SelectData(string requestKey, params string[] parvls)
+        public object SelectData(string requestKey, Dictionary<string,string> @params = null)
         {
             return Context(db =>
             {
@@ -16,14 +17,18 @@ namespace Server.DBLocal
                 {
                     data = JsonConvert.DeserializeObject(db.DDatas.Where(i => i.Did.Equals(api.Did)).FirstOrDefault().Data);
                 }
-                else
+                else if (api.Pattern.Equals("即时查询"))
                 {
                     string commandText = db.DMsSQLs.Where(i => i.Sid.Equals(api.Sid)).FirstOrDefault().Sql;
-                    
+                    data = null;
+                }
+                else
+                {
+                    data = null;
                 }
 
 
-                //return data;
+                return data;
             });
         }
     }
